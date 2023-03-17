96f383ecb181bd5d802f70f15050bbacc0c9da5d
Rupam Gupta <guptarupam.1020@gmail.com>
2023-03-17 12:27:53 +0530

    Folder Structure Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GetBook from "./pages/book/GetBook";
+import Register from "./pages/register/Register";
+import Footer from "./components/footer/Footer";
 
-function App() {
+const App = () => {
+  const token = JSON.parse(localStorage.getItem("data"));
+  console.log(token);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar token={token} />
+        <Routes>
+          <Route path="/" element={<Home token={token} />}></Route>
+          <Route path="/Register" element={<Register />}></Route>
+          {token ? (
+            <Route path="/" element={<Home />}></Route>
+          ) : (
+            <Route path="/Login" element={<Login />}></Route>
+          )}
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/GetBook" element={<GetBook />}></Route>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
